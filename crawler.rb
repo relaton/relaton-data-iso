aa7c7f1f04d7b9b8babbfd4e6542665c0bcd9837
Andrei Kislichenko <android.2net@gmail.com>
2024-05-15 15:24:01 -0400

    fix index

--- crawler.rb
+++ crawler.rb
@@ -8,13 +8,13 @@ RelatonIso::DataFetcher.fetch
 index_file = RelatonIso::HitCollection::INDEXFILE
 index = Relaton::Index.find_or_create :iso, file: index_file
 
-# Dir["data/**/*.yaml"].each do |f|
-#   hash = YAML.load_file f
-#   item_hash = RelatonIso::HashConverter.hash_to_bib(hash)
-#   bib = RelatonIsoBib::IsoBibliographicItem.new(**item_hash)
-#   id = bib.docidentifier.detect(&:primary)
-#   index.add_or_update id.to_h, f
-# end
+Dir["data/**/*.yaml"].each do |f|
+  hash = YAML.load_file f
+  item_hash = RelatonIso::HashConverter.hash_to_bib(hash)
+  bib = RelatonIsoBib::IsoBibliographicItem.new(**item_hash)
+  id = bib.docidentifier.detect(&:primary)
+  index.add_or_update id.to_h, f
+end
 
 Dir["static/**/*.yaml"].each do |f|
   hash = YAML.load_file f
