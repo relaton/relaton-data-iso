b5ca282aec2b7244f07167fbc80de5daec684e97
Andrei Kislichenko <android.2net@gmail.com>
2024-05-30 13:23:57 -0400

    start parsing

--- crawler.rb
+++ crawler.rb
@@ -3,26 +3,26 @@
 require 'relaton_iso'
 
 # Run data fetcher
-# RelatonIso::DataFetcher.fetch
+RelatonIso::DataFetcher.fetch
 
-# index_file = RelatonIso::HitCollection::INDEXFILE
-# index = Relaton::Index.find_or_create :iso, file: index_file
+index_file = RelatonIso::HitCollection::INDEXFILE
+index = Relaton::Index.find_or_create :iso, file: index_file
 
-# Dir["data/**/*.yaml"].each do |f|
-#   hash = YAML.load_file f
-#   item_hash = RelatonIso::HashConverter.hash_to_bib(hash)
-#   bib = RelatonIsoBib::IsoBibliographicItem.new(**item_hash)
-#   id = bib.docidentifier.detect(&:primary)
-#   index.add_or_update id.to_h, f
-# end
+Dir["data/**/*.yaml"].each do |f|
+  hash = YAML.load_file f
+  item_hash = RelatonIso::HashConverter.hash_to_bib(hash)
+  bib = RelatonIsoBib::IsoBibliographicItem.new(**item_hash)
+  id = bib.docidentifier.detect(&:primary)
+  index.add_or_update id.to_h, f
+end
 
-# Dir["static/**/*.yaml"].each do |f|
-#   hash = YAML.load_file f
-#   item_hash = RelatonIso::HashConverter.hash_to_bib(hash)
-#   bib = RelatonIsoBib::IsoBibliographicItem.new(**item_hash)
-#   id = bib.docidentifier.detect(&:primary)
-#   index.add_or_update id.to_h, f
-# end
-# index.save
+Dir["static/**/*.yaml"].each do |f|
+  hash = YAML.load_file f
+  item_hash = RelatonIso::HashConverter.hash_to_bib(hash)
+  bib = RelatonIsoBib::IsoBibliographicItem.new(**item_hash)
+  id = bib.docidentifier.detect(&:primary)
+  index.add_or_update id.to_h, f
+end
+index.save
 
-# `git add #{RelatonIso::Queue::FILE}`
+`git add #{RelatonIso::Queue::FILE}`
