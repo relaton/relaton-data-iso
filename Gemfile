dafd19bbde3cacbe012a5b520a365491c587b1b2
Andrei Kislichenko <android.2net@gmail.com>
2025-04-01 00:29:41 -0400

    update crawler

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ source "https://rubygems.org"
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-gem "relaton-iso", github: "relaton/relaton-iso", branch: "main"
+gem "relaton-iso", "~> 2.0.0-alpha.1" #, github: "relaton/relaton-iso", branch: "main"
--- crawler.rb
+++ crawler.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
-require "relaton_iso"
+require "relaton/iso/data_fetcher"
 
 ENV["GITHUB_TOKEN"] = ARGV.last if ARGV.last
 
-# Run data fetcher
-RelatonIso::DataFetcher.fetch
+# Run data fetcher. It will download 10,000 ISO standards and
+# save them to the default data directory in default YAML format.
+# Next run it will download next 10,000 standards.
+Relaton::Iso::DataFetcher.fetch
 
-index_file = RelatonIso::HitCollection::INDEXFILE
+index_file = "#{Relaton::Iso::HitCollection::INDEXFILE}.yaml"
 index = Relaton::Index.find_or_create :iso, file: index_file
 
 Dir["data/**/*.yaml"].each do |f|
-  hash = YAML.load_file f
-  item_hash = RelatonIso::HashConverter.hash_to_bib(hash)
-  bib = RelatonIsoBib::IsoBibliographicItem.new(**item_hash)
-  id = bib.docidentifier.detect(&:primary)
+  item = Relaton::Iso::Item.from_yml File.read(f, encoding: "UTF-8")
+  id = item.docidentifier.detect(&:primary)
   index.add_or_update id.to_h, f
 end
 
 Dir["static/**/*.yaml"].each do |f|
-  hash = YAML.load_file f
-  item_hash = RelatonIso::HashConverter.hash_to_bib(hash)
-  bib = RelatonIsoBib::IsoBibliographicItem.new(**item_hash)
-  id = bib.docidentifier.detect(&:primary)
+  item = Relaton::Iso::Item.from_yml File.read(f, encoding: "UTF-8")
+  id = item.docidentifier.detect(&:primary)
   index.add_or_update id.to_h, f
 end
 index.save
 
-`git add #{RelatonIso::Queue::FILE}`
+`git add #{Relaton::Iso::Queue::FILE}`
